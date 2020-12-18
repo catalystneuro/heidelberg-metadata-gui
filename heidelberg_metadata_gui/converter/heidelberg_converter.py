88f674bc01ba2ac68d9ddbb28c99119e1031bdd4
luiz <luiz_tauffer@hotmail.com>
2020-12-18 16:31:50 +0100

    use CEDRecordingInterface

--- heidelberg_metadata_gui/converter/heidelberg_converter.py
+++ heidelberg_metadata_gui/converter/heidelberg_converter.py
@@ -1,5 +1,4 @@
-from nwb_conversion_tools import NWBConverter
-
+from nwb_conversion_tools import NWBConverter, CEDRecordingInterface
 from .heidelberg_ophys_interface import HeidelbergOphysInterface
 from .heidelberg_ecephys_interface import HeidelbergEcephysInterface
 
@@ -8,9 +7,16 @@ class HeidelbergNWBConverter(NWBConverter):
 
     # Modular data interfaces
     data_interface_classes = {
-        'HeidelbergEcephysInterface': HeidelbergEcephysInterface,
+        # 'HeidelbergEcephysInterface': HeidelbergEcephysInterface,
+        'CEDRecording': CEDRecordingInterface,
         'HeidelbergOphysInterface': HeidelbergOphysInterface,
     }
 
     def __init__(self, source_data):
+        channel_info = CEDRecordingInterface.get_all_channels_info(source_data['CEDRecording']['file_path'])
+        rhd_channels = []
+        for ch, info in channel_info.items():
+            if "Rhd" in info["title"]:
+                rhd_channels.append(ch)
+        source_data['CEDRecording'].update(smrx_channel_ids=rhd_channels)
         super().__init__(source_data)
